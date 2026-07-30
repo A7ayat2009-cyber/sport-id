3e16b862cd53881e5a2ab2fbbaeb4b25daaae445
A7ayat2009-cyber <A7ayat2009@icloud.com>
2026-07-30 23:53:30 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkuMPsXEdgPmumEvy-1nYsnQUzaxZY714",
+  authDomain: "alza3eem-913ed.firebaseapp.com",
+  projectId: "alza3eem-913ed",
+  storageBucket: "alza3eem-913ed.firebasestorage.app",
+  messagingSenderId: "412892587540",
+  appId: "1:412892587540:web:333cd572e72eb8f972a901"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
