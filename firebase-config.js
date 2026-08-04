5e3469275c7b1a6f9837f785a20e17cc3c68ce6e
alza3eem <A7ayat2009@icloud.com>
2026-08-04 23:46:44 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,6 +4,8 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDkuMPsXEdgPmumEvy-1nYsnQUzaxZY714",
   authDomain: "alza3eem-913ed.firebaseapp.com",
@@ -17,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
